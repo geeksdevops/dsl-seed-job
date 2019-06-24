71bfbe12574a99a22eab8e8014dd6982ebb9350c
Pramod <pramod.s.02@gmail.com>
2019-06-25 02:01:29 +0530

    added CoberturaReport.groovy and Jenkinsfile

---
+++ src/jobs/CoberturaReport.groovy
@@ -0,0 +1,26 @@
+String basePath = 'CoberturaReport'
+String repo = 'geeksdevops/Clock-CoberturaReport'
+
+folder(basePath) {
+    description 'This example shows Clock-CoberturaReport basic folder/job creation.'
+}
+
+job("$basePath/ReportCobertura") {
+    scm {
+        github repo
+    }
+    triggers {
+        scm 'H/5 * * * *'
+    }
+    steps {
+	shell 'echo "Publishing Report on board"'
+}
+    definition {
+        cps {
+            script(readFileFromWorkspace('Jenkinsfile'))
+            sandbox()
+        }
+    }
+
+}
+
